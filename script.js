9d8e6eef0d971b3a36fa3cce4c4a9062a9adbd3d
Sofia <sofiamironeko@gmail.com>
2022-11-23 12:05:29 +1100

    simplfied clock funcition

--- script.js
+++ script.js
@@ -1,23 +1,13 @@
-// show time and date that updates every second
-function showTime() {
-    let date = new Date();
-    let hours = date.getHours();
-    let minutes = date.getMinutes();
-    let seconds = date.getSeconds();
-    let day = date.getDay();
-    let month = date.getMonth();
-    let year = date.getFullYear();
-    let ampm = hours >= 12 ? 'pm' : 'am';
-    hours = hours % 12;
-    hours = hours ? hours : 12; 
-    minutes = minutes < 10 ? '0'+minutes : minutes;
-    seconds = seconds < 10 ? '0'+seconds : seconds;
-    let strTime = hours + ':' + minutes + ':' + seconds + ' ' + ampm;
-    document.getElementById("time").innerHTML = strTime;
-    document.getElementById("date").innerHTML = day + '/' + month + '/' + year;
-    setTimeout(showTime, 1000);
+// update users time and date on the page
+function updateClock() {
+    let now = new Date();
+    let date = now.toDateString();
+    let time = now.toLocaleTimeString();
+    document.getElementById('date').innerHTML = date;
+    document.getElementById('time').innerHTML = time;
+    setInterval(updateClock, 1000);
+
 }
-// show time on website
-showTime();
+updateClock();
 
 
