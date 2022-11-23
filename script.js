86aa7803c1f0474cf191aea5f9abe2488e6adfc6
Sofia <sofiamironeko@gmail.com>
2022-11-23 19:23:27 +1100

    Made Create a new task button "model"  input form

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 // update users time and date on the page
 function updateClock() {
-    let now = new Date();
-    let date = now.toDateString();
-    let time = now.toLocaleTimeString();
-    document.getElementById('date').innerHTML = date;
-    document.getElementById('time').innerHTML = time;
-    setInterval(updateClock, 1000);
-
+  let now = new Date();
+  let date = now.toDateString();
+  let time = now.toLocaleTimeString();
+  document.getElementById("date").innerHTML = date;
+  document.getElementById("time").innerHTML = time;
+  setInterval(updateClock, 1000);
 }
+
 updateClock();
 
+// function validate from
 
