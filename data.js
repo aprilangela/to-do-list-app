6fbcfa2adcc1d701f1b89ce5cd3d534ff6a73a66
Sofia <sofiamironeko@gmail.com>
2022-12-01 08:39:51 +1100

    Turned in to class and methods

---
+++ data.js
@@ -0,0 +1,104 @@
+export class taskMaster {
+
+  //define object structure ID = int , task = string, status = string  DueDate date it is due  AssignedTo  description 
+//define a task 
+  constructor() {
+    this.tasklist = [];
+    this.load();
+  }
+  
+  createHtmlCard(task) {
+    //create a card
+    document.getElementById("task_list").innerHTML += `
+
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">${task.task}</h5>
+
+        <div class="card-body">
+        <p class="card-text">Description: ${task.description}</p>
+          <p class="card-text">due date: ${task.dueDate}</p>
+          <p class="card-text">Assigned to: ${task.assignedTo}</p> 
+          <p class="card-text">Status: ${task.status}</p>
+
+          
+          <i onclick="taskMaster.editTask(${task.id})" data-bs-toggle="modal" data-bs-target="#form" class="fas fa-edit"></i>
+          <i id="(${task.id}icon" onClick ="deleteTask(${task.id})" class="fas fa-trash-alt"></i>
+          <i onClick ="" class="fas fa-check"></i>
+          
+          </div>
+        </div>
+      </div>
+            </div>
+    </div>
+    `;
+  }
+  
+  render()  {
+    this.tasklist.forEach(task => this.createHtmlCard(task));
+  }
+
+  addTask(task) { 
+    this.tasklist.push(task);
+    this.save(); 
+  }
+save () {
+  localStorage.setItem("tasklist", JSON.stringify(this.tasklist));
+}
+
+load () {
+  this.tasklist = JSON.parse(localStorage.getItem("tasklist"));
+  if (this.tasklist == null) {
+    this.tasklist = [];
+  }
+}
+
+
+  getAllTasks() {
+    return this.tasklist;
+
+  }
+  getTasksWithStatus(status)
+  {
+   this.tasklist.filter(task => task.status === status);
+
+    if (status == "Done") {
+      document.getElementById("(${task.id}icon").style.display = "none";
+
+    }
+
+
+  }
+
+  
+  
+   markDone(taskId) {
+    let task = this.tasklist.find(task => task.id === taskId);
+    task.status = "Done";
+    this.save();
+    
+  }
+
+
+
+editTask(taskId) {
+  let task = this.tasklist.find(task => task.id === taskId);
+  document.getElementById("name-task-input").value = task.task;
+  document.getElementById("description-task-input").value = task.description;
+  document.getElementById("assigned-to-task-input").value = task.assignedTo;
+  document.getElementById("due-date-task-input").value = task.dueDate;
+  document.getElementById("status-task-input").value = task.status;
+  document.getElementById("id-task-input").value = task.id;
+  document.getElementById("Modal-label1").innerHTML = "Edit Task";
+  document.getElementById("submit").innerHTML = "Edit Task";
+  document.getElementById("submit").onclick = function() {taskMaster.updateTask(taskId)};
+
+
+}
+deleteTask(taskId) {
+  let task = this.tasklist.find(task => task.id === taskId);
+  this.tasklist.splice(task, 1);
+  this.save();
+}
+}
+
--- script.js
+++ script.js
@@ -1,25 +1,50 @@
-  import "./modules/taskMaster.js";
+import { taskMaster } from './data.js';
+let form = document.querySelector("#form");
 
-let form = document.getElementById("form");
-let taskNameInput = document.getElementById("name-task-input");
-let descriptionInput = document.getElementById("description-task-input");
-let assignedToInput = document.getElementById("assigned-to-task-input");
-let dueDateInput = document.getElementById("due-date-task-input");
-let statusInput = document.getElementById("status-task-input");
-let tasks = document.getElementById("tasks");
-let add = document.getElementById("add");
+const tskmst = new taskMaster();
 
+
+
+console.log(tskmst.tasklist);
+tskmst.render();
+
+//add new task 
+
+
+function updateClock() {
+  let now = new Date();
+  let date = now.toDateString();
+  let time = now.toLocaleTimeString();
+  document.getElementById("date").innerHTML = date;
+  document.getElementById("time").innerHTML = time;
+  setInterval(updateClock, 1000);
+
+}
+
+updateClock();
+
+
+
+
+
+//  Input the data from form, Validate the data and store that data in object format in local storage
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  formValidation();
-});
+  let taskName = document.getElementById("name-task-input").value;
+  let taskDescription = document.getElementById("description-task-input").value;
+  let taskAssignedTo = document.getElementById("assigned-to-task-input").value;
+  let taskDueDate = document.getElementById("due-date-task-input").value;
+  let taskStatus = document.getElementById("status-task-input").value;
+  let task = {
+    task: taskName,
+    description: taskDescription,
+    assignedTo: taskAssignedTo,
+    dueDate: taskDueDate,
+    status: taskStatus,
+  };
 
-
-("use strict");
-
-// validation function for the form inputs and error messages for each input field if not valid and if valid it will add the task to the array and local storage and clear the form
-let formValidation = () => {
-  if (taskNameInput.value.length < 8) {
+  if (taskName.length < 8) {
+    document.getElementById("name-task-input").focus
     document.getElementById("name-task-error").innerHTML =
       "Please enter a task name thats at least 8 characters long";
     document.getElementById("name-task-input").style.borderColor = "red";
@@ -32,7 +57,7 @@ let formValidation = () => {
     document.getElementById("name-task-error").innerHTML = "";
   }
 
-  if (descriptionInput.value.length < 15) {
+  if (taskDescription.length < 15) {
     document.getElementById("description-task-error").innerHTML =
       "Please enter a description thats at least 15 characters long";
     document.getElementById("description-task-input").style.borderColor = "red";
@@ -50,7 +75,7 @@ let formValidation = () => {
     document.getElementById("description-task-error").innerHTML = "";
   }
 
-  if (assignedToInput.value.length < 8) {
+  if (taskAssignedTo.length < 8) {
     document.getElementById("assigned-to-task-error").innerHTML =
       "Please enter a name thats at least 8 characters long";
     document.getElementById("assigned-to-task-input").style.borderColor = "red";
@@ -67,7 +92,7 @@ let formValidation = () => {
       "green";
     document.getElementById("assigned-to-task-error").innerHTML = "";
   }
-  if (dueDateInput.value < new Date().toISOString().split("T")[0]) {
+  if (taskDueDate < new Date().toISOString().split("T")[0]) {
     document.getElementById("due-date-task-input").style.borderColor = "red";
     document.getElementById("due-date-task-error").innerHTML =
       "Due date must be today or later";
@@ -82,7 +107,8 @@ let formValidation = () => {
     document.getElementById("due-date-task-error").innerHTML = "";
   }
 
-  if (statusInput.value === "0") {
+  if (taskStatus.length == "1") {
+
     document.getElementById("status-task-input").style.borderColor = "red";
     document.getElementById("status-task-error").innerHTML =
       "Please select a status";
@@ -92,42 +118,30 @@ let formValidation = () => {
     document.getElementById("status-task-input").classList.add("is-valid");
     document.getElementById("status-task-input").style.borderColor = "green";
     document.getElementById("status-task-error").innerHTML = "";
+
   }
 
   if (
-    taskNameInput.value.length > 7 &&
-    descriptionInput.value.length > 14 &&
-    assignedToInput.value.length > 7 &&
-    dueDateInput.value > new Date().toISOString().split("T")[0] &&
-    statusInput.value !== "0"
+    taskName.length >= 8 &&
+    taskDescription.length >= 15 &&
+    taskAssignedTo.length >= 8 &&
+    taskDueDate >= new Date().toISOString().split("T")[0] &&
+    taskStatus.value != "0"
   ) {
-    acceptData();
+
+    tskmst.addTask(task);
+    tskmst.render();
     clearForms();
-    add.setAttribute("data-bs-dismiss", "modal");
-    add.click();
-
-    (() => {
-      add.setAttribute("data-bs-dismiss", "");
-    })();
+    location.reload();
+    return true;
   }
-};
-
-// this makes object and pushes it to array and local storage
+});
 
 
-// run clearforms function when modal is closed
 
-// reset form  function
-let resetForm = () => {
-  form.reset();
-};
-// render function parses data and renders it to the DOM
-(() => {
-  data = JSON.parse(localStorage.getItem("data")) || [];
-  createTasks();
-})();
 
-// clears all the inputs
+
+
 function clearForms() {
   document.querySelector("#form").reset();
   document.getElementById("name-task-error").innerHTML = "";
@@ -173,29 +187,3 @@ const reset = document.querySelector("#btnReset");
 reset.addEventListener("click", function () {
   clearForms();
 });
-// close button for modal form clears the form
-const close = document.querySelector("#btnClose");
-close.addEventListener("click", function () {
-  clearForms();
-});
-
-function addNewTask() {
-  document.getElementById("Modal-label1").innerHTML = "Add New Task";
-  clearForms();
-}
-
-// open delete modal
-
-
-function updateClock() {
-  let now = new Date();
-  let date = now.toDateString();
-  let time = now.toLocaleTimeString();
-  document.getElementById("date").innerHTML = date;
-  document.getElementById("time").innerHTML = time;
-  setInterval(updateClock, 1000);
-
-}
-
-updateClock();
-
